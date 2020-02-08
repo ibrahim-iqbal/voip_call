1869aef0aaf6653e0c207b837df9392b300e1879
ibrahim-iqbal <ibiq1630@gmail.com>
2020-02-08 11:59:02 +0530

    chat insert module

--- app/src/main/java/com/example/voip_call/ChatFragment.java
+++ app/src/main/java/com/example/voip_call/ChatFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -16,11 +17,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
 public class ChatFragment extends Fragment {
     String userEmail;
@@ -33,7 +36,6 @@ public class ChatFragment extends Fragment {
     String id, massage, name, Id, img;
     long time;
     Context context;
-
     public ChatFragment(Context mcontext) {
         // Required empty public constructor
         context = mcontext;
@@ -46,11 +48,13 @@ public class ChatFragment extends Fragment {
 
         mauth = FirebaseAuth.getInstance();
         r_db = FirebaseDatabase.getInstance().getReference("userinfo");
-        userEmail=mauth.getCurrentUser().getEmail();
+        db = FirebaseDatabase.getInstance().getReference("chatinfo");
+        userEmail = mauth.getCurrentUser().getEmail();
 
         recyclerView = v.findViewById(R.id.recic);
         layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
+//        Toast.makeText(context, ""+mauth.getCurrentUser().get, Toast.LENGTH_SHORT).show();
 
         return v;
     }
@@ -59,23 +63,29 @@ public class ChatFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        r_db.addValueEventListener(new ValueEventListener() {
+
+        userlist = new ArrayList<>();
+
+        r_db.orderByChild("email").equalTo(mauth.getCurrentUser().getEmail()).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                userlist = new ArrayList<>();
-                for (DataSnapshot data : dataSnapshot.getChildren()) {
-                    String name = Objects.requireNonNull(data.child("name").getValue()).toString();
-                    String img = Objects.requireNonNull(data.child("imgurl").getValue()).toString();
-                    String email = Objects.requireNonNull(data.child("email").getValue()).toString();
+                for (DataSnapshot datas : dataSnapshot.getChildren()) {
+//                    String name = datas.child("name").getValue().toString();
+//                    String email = datas.child("email").getValue().toString();
+//                    String imgurl = datas.child("imgurl").getValue().toString();
+                    id = datas.child("id").getValue().toString();
+//                    u_name.setText(name);
+//                    u_email.setText(email);
+//                    if (imgurl.equals("")) {
+//                        imgurl = "jjd";
+//                    } else {
+//                        Picasso.get().load(imgurl).into(profileimg);
+//                    }
+//                    Toast.makeText(context, "" + id, Toast.LENGTH_SHORT).show();
+                    userlist.clear();
+                    chatuser(id);
 
-                    if (email.equals(userEmail)) {
-                        img = "lkk";
-                    } else {
-                        userlist.add(new UserinfoList(img, name, email));
-                    }
                 }
-                madpter = new Chat_RecyclerView_Adpter(Objects.requireNonNull(getContext()), userlist);
-                recyclerView.setAdapter(madpter);
             }
 
             @Override
@@ -83,5 +93,144 @@ public class ChatFragment extends Fragment {
 
             }
         });
+
+//        r_db.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                userlist = new ArrayList<>();
+//                for (DataSnapshot data : dataSnapshot.getChildren()) {
+//                    String name = Objects.requireNonNull(data.child("name").getValue()).toString();
+//                    String img = Objects.requireNonNull(data.child("imgurl").getValue()).toString();
+//                    String email = Objects.requireNonNull(data.child("email").getValue()).toString();
+//
+//                    if (email.equals(userEmail)) {
+//                        img = "lkk";
+//                    } else {
+//                        userlist.add(new UserinfoList(img, name, email));
+//                    }
+//                }
+//                madpter = new Chat_RecyclerView_Adpter(Objects.requireNonNull(getContext()), userlist);
+//                recyclerView.setAdapter(madpter);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+    }
+
+
+    public void chatuser(final String id) {
+
+        db.child(id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (final DataSnapshot da : dataSnapshot.getChildren()) {
+                    String chatid = da.getKey();
+//                    Toast.makeText(context, ""+chatid, Toast.LENGTH_SHORT).show();
+                    Query query2 = db.child(id).child(chatid);
+                    query2.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            int no = 0;
+                            for (DataSnapshot unda : dataSnapshot.getChildren()) {
+                                if (Integer.parseInt(unda.child("unread").getValue().toString()) >= 0) {
+                                    no++;
+                                }
+                            }
+                            Toast.makeText(context, "" + no, Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                    Query query = db.child(id).child(chatid).limitToLast(1).orderByKey();
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot lchat : dataSnapshot.getChildren()) {
+                                massage = lchat.child("massage").getValue().toString();
+                                time = (long) lchat.child("time").getValue();
+                                String s_Id = lchat.child("senderId").getValue().toString();
+                                String re_Id = lchat.child("reciverId").getValue().toString();
+                                String show_userid;
+                                if (s_Id.equals(id)) {
+                                    show_userid = re_Id;
+                                } else {
+                                    show_userid = s_Id;
+                                }
+                                getuserinfo(show_userid, massage, time);
+
+
+                                Toast.makeText(context, "" + show_userid, Toast.LENGTH_SHORT).show();
+                                Toast.makeText(context, "last" + massage, Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }
+
+    public void getuserinfo(final String userid, String massage, long tm) {
+
+        final String mass = massage;
+        final long tmm = tm;
+        r_db.orderByKey().equalTo(userid).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot userda : dataSnapshot.getChildren()) {
+                    name = userda.child("name").getValue().toString();
+                    img = userda.child("imgurl").getValue().toString();
+                    Id = userda.child("id").getValue().toString();
+                    userlist.add(new UserinfoList(img, name, mass, Id, tmm));
+                }
+
+//                sort list according to sending and receiving massage
+                Collections.sort(userlist, new Comparator<UserinfoList>() {
+                    @Override
+                    public int compare(UserinfoList o1, UserinfoList o2) {
+                        int tm1 = (int) o1.getTm();
+                        int tm2 = (int) o2.getTm();
+
+                        if (tm1 < tm2) {
+                            return 1;
+                        } else {
+                            return -1;
+                        }
+
+                    }
+                });
+                madpter = new Chat_RecyclerView_Adpter(context, userlist);
+                recyclerView.setAdapter(madpter);
+//                madpter.notifyItemChanged(1);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
