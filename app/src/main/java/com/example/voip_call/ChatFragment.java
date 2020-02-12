087fed3ce3b0725906e36c6cd36dddd06e772e25
ibrahim-iqbal <ibiq1630@gmail.com>
2020-02-12 10:35:10 +0530

    chat insert module

--- app/src/main/java/com/example/voip_call/ChatFragment.java
+++ app/src/main/java/com/example/voip_call/ChatFragment.java
@@ -137,8 +137,6 @@ public class ChatFragment extends Fragment {
                                                     }
 
                                                 });
-
-
                                             }
 
                                         }
